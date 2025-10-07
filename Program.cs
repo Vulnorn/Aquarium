85cc044c6081a9fb0db89aaa76e21c76139b2097
Vulnorn <mixadrbk31@gmail.com>
2025-10-07 17:09:23 +0300

    Не работает удаление при смерти

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using static System.Net.Mime.MediaTypeNames;
-using System.Xml.Linq;
 
 namespace Aquarium
 {
@@ -18,6 +16,7 @@ namespace Aquarium
     public class Aquarium
     {
         private AquariumFactory _factory = new AquariumFactory();
+        private FishFactory _fishFactory = new FishFactory();
 
         private const int MaxFishes = 10;
         private List<Fish> _fishes;
@@ -72,7 +71,6 @@ namespace Aquarium
                 GrowOldFishes();
                 RemovedDeadFishes();
 
-
                 Console.ReadKey();
                 Console.Clear();
             }
@@ -85,9 +83,9 @@ namespace Aquarium
 
         private void ShowFishes()
         {
-            foreach (Fish fish in _fishes)
+            for (int i = 0; i < _fishes.Count; i++)
             {
-                Console.WriteLine($"Рыба #{fish.Name} - возраст: {fish.Age}");
+                Console.WriteLine($"{i + 1}) Рыба {_fishes[i].Name} - возраст: {_fishes[i].Age}");
             }
         }
 
@@ -102,7 +100,11 @@ namespace Aquarium
         private void AddFish()
         {
             if (_fishes.Count < MaxFishes)
-                _fishes.Add(new Fish());
+            {
+                _fishes.Add((_fishFactory.GetList()[Utilite.GenerateRandomNumber(0, _fishFactory.GetList().Count)].Clone()));
+                Console.WriteLine($"Была добавлена {_fishes[_fishes.Count-1].Name} рыба с возрастом {_fishes[_fishes.Count-1].Age}");
+            }
+
             else
                 Console.WriteLine("В аквариуме нет места");
         }
@@ -111,47 +113,46 @@ namespace Aquarium
         {
             if (_fishes.Count > 0)
             {
-                ShowFishes();
+                Console.WriteLine("Введите номер рыбы которую хотите удалить:");
 
-                Console.WriteLine("Введите название рыбы которую хотите удалить:");
+                int number = Utilite.GetNumberInRange(1, _fishes.Count);
 
-                if (int.TryParse(Console.ReadLine(), out int index))
-                {
-                    foreach (Fish fish in _fishes)
-                    {
-                        if (fish.Name == index)
-                        {
-                            _fishes.Remove(fish);
-                            return;
-                        }
-                    }
-                }
+                _fishes.Remove(_fishes[number - 1]);
             }
-
-            if (_fishes.Count == 0)
+            else
             {
                 Console.WriteLine("В аквариуме нет рыбок");
-                return;
             }
         }
 
         private void RemovedDeadFishes()
         {
-            for (int i = _fishes.Count - 1; i > -1; i--)
+            for (int i = 0; i > _fishes.Count; i++)
             {
-                if (_fishes[i].IsDead)
+                if (_fishes[i].IsDead())
                 {
-                    Console.WriteLine($"Удалена рыба с ID: {_fishes[i].Name}. Срок жизни был {_fishes[i].MaxLife}");
+                    Console.WriteLine($"Удалена рыба {_fishes[i].Name}. Срок жизни был {_fishes[i].MaxLife}");
+
                     _fishes.RemoveAt(i);
+
+                    i--;
+                }
+                else
+                {
+                    Console.WriteLine($"Никто нре умер");
                 }
             }
+
+            Console.ReadKey();
         }
     }
 
     public class AquariumFactory
     {
-        private List<Fish> _aquarium =new List<Fish>();
+        private FishFactory _fishFactory = new FishFactory();
 
+        private List<Fish> _aquarium = new List<Fish>();
+      
         public AquariumFactory()
         {
             Create();
@@ -159,12 +160,10 @@ namespace Aquarium
 
         private void Create()
         {
-            _aquarium.Add(new Fish("Скалярия",6));
-            _aquarium.Add(new Fish("Золотая рыбка",5));
-            _aquarium.Add(new Fish("Гурами", 8));
-            _aquarium.Add(new Fish("Макропод", 7));
-            _aquarium.Add(new Fish("Попугай", 10));
-
+            for (int i = 0; i < _fishFactory.GetCount(); i++)
+            {
+                _aquarium.Add(_fishFactory.GetList()[i].Clone());
+            }
         }
 
         public List<Fish> GetList()
@@ -173,46 +172,110 @@ namespace Aquarium
         }
     }
 
+    public class FishFactory
+    {
+        private List<Fish> _fish = new List<Fish>();
+
+        public FishFactory()
+        {
+            Create();
+        }
+
+        private void Create()
+        {
+            _fish.Add(new Fish("Скалярия", 6));
+            _fish.Add(new Fish("Золотая рыбка", 5));
+            _fish.Add(new Fish("Гурами", 8));
+            _fish.Add(new Fish("Макропод", 7));
+            _fish.Add(new Fish("Попугай", 10));
+        }
+
+        public List<Fish> GetList()
+        {
+            return _fish;
+        }
+
+        public int GetCount()
+        {
+            return _fish.Count;
+        }
+    }
+
     public class Fish
     {
         private const int MinAge = 1;
-        private static int s_id = 1;
 
-        public Fish(string name,int maxLife)
+        public Fish(string name, int maxLife)
         {
-            Name = name; 
+            Name = name;
             MaxLife = maxLife;
-            Age = UserUtils.GenerateRandomNumber(MinAge, maxLife);
+            Age = Utilite.GenerateRandomNumber(MinAge, maxLife);
         }
 
         public string Name { get; private set; }
         public int Age { get; private set; }
         public int MaxLife { get; private set; }
-        public bool IsDead => Age >= MaxLife;
 
         public void GrowOld()
         {
             Age++;
         }
 
-        private int NewId()
-        {
-            return s_id++;
-        }
-
         public Fish Clone()
         {
-            return new Fish(Name,MaxLife);
+            return new Fish(Name, MaxLife);
+        }
+
+        public bool IsDead()
+        {
+            return Age>=MaxLife;
         }
     }
 
-    public class UserUtils
+    class Utilite
     {
         private static Random s_random = new Random();
 
-        public static int GenerateRandomNumber(int minValue, int maxValue)
+        public static int GenerateRandomNumber(int lowerLimitRangeRandom, int upperLimitRangeRandom)
         {
-            return s_random.Next(minValue, maxValue);
+            return s_random.Next(lowerLimitRangeRandom, upperLimitRangeRandom);
+        }
+
+        public static int GetNumberInRange(int lowerLimitRangeNumbers = Int32.MinValue, int upperLimitRangeNumbers = Int32.MaxValue)
+        {
+            bool isEnterNumber = true;
+            int enterNumber = 0;
+            string userInput;
+
+            while (isEnterNumber)
+            {
+                Console.WriteLine($"Введите число.");
+
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out enterNumber) == false)
+                    Console.WriteLine("Не корректный ввод.");
+                else if (VerifyForAcceptableNumber(enterNumber, lowerLimitRangeNumbers, upperLimitRangeNumbers))
+                    isEnterNumber = false;
+            }
+
+            return enterNumber;
+        }
+
+        private static bool VerifyForAcceptableNumber(int number, int lowerLimitRangeNumbers, int upperLimitRangeNumbers)
+        {
+            if (number < lowerLimitRangeNumbers)
+            {
+                Console.WriteLine($"Число вышло за нижний предел допустимого значения.");
+                return false;
+            }
+            else if (number > upperLimitRangeNumbers)
+            {
+                Console.WriteLine($"Число вышло за верхний предел допустимого значения.");
+                return false;
+            }
+
+            return true;
         }
     }
 }
